8e8043611b30d82aa40a1d227c6946acba1cdee5
Gervasio Neto <gervasiolopesn@gmail.com>
2024-11-22 18:00:27 -0300

    Progresso no trabalho de POO

--- src/app/Main.java
+++ src/app/Main.java
@@ -1,7 +1,14 @@
 package app;
 
+import entities.Cliente;
+
+import java.util.Arrays;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-
+        List<String> preferenciasC1 = Arrays.asList("Vista para o mar", "Quarto silencioso");
+        Cliente c1 = new Cliente("Maria Souza", "987.654.321-00", "(11) 99876-5432", 56789, preferenciasC1);
+        c1.exibirInformacoes();
     }
 }
